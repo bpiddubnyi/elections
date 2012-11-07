b971c05a5a5ba9a6dd72ed8d028e2014a5e94e35
Borys Piddubnyi <zhu@zhu.org.ua>
2012-11-07 23:30:18 +0200

    License added

--- elections.go
+++ elections.go
@@ -1,3 +1,20 @@
+/*
+Copyright 2012 Borys Piddubnyi <zhu@zhu.org.ua>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
@@ -12,15 +29,15 @@ import (
 
 const (
 	defaultPrecision = 10
-	defaultPrePath = "plots"
+	defaultPrePath   = "plots"
 )
 
 func main() {
 	/* Flag parsing*/
 	var (
 		verbose, help bool
-		precision int
-		plotPath string
+		precision     int
+		plotPath      string
 	)
 
 	defaultPath := path.Join(defaultPrePath, "<precision>")
